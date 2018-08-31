13bf32d4905202031701eb946928b9386a2a5a75
Emerson Demetrio <emerson.demetrio@scytech.com.br>
2018-08-31 17:31:47 -0300

    Updating messages

--- copy.sh
+++ copy.sh
@@ -3,11 +3,12 @@
 # sed https://askubuntu.com/questions/344407/how-to-read-complete-line-in-for-loop-with-spaces
 # for https://unix.stackexchange.com/questions/271506/remove-lines-that-contain-file-path-using-sed
 
-IFS=$'\n'
-for al in $(cat ./aliases.conf);
+IFS=$'\n';
+
+for entry in $(cat ./aliases.conf);
 do
-	echo "removing '$al'";
-	sudo sed --in-place '\|'"$al"'|d' ~/.bashrc;
+	echo "Entry: '$entry'";
+	sudo sed --in-place '\|'"$entry"'|d' ~/.bashrc;
 done;
 
 sudo cat ./aliases.conf >> ~/.bashrc;
